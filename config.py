699cfc91df51dc08b65745c4cb712e47ea1c14d4
Yasarali23 <128029707+Yasarali23@users.noreply.github.com>
2025-07-02 23:42:56 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+# Use your actual bot token or use Render env
+TOKEN = os.getenv("TOKEN", "7840887285:AAEYjOZSSrcM55cT6mcivXOrozlWfucgS1s")
+
+# Admins by Telegram ID
+ADMINS = [5720602195]
